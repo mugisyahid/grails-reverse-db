e7ca058d3cea9e570bafce007edb8e127391ae11
Ligar Mugi Syahid <mugisyahid@gmail.com>
2018-05-22 20:18:47 +0700

    first blood

---
+++ README.md
@@ -0,0 +1,4 @@
+# Grails 3.0.11 DB reverse engineering
+
+
+Example of org.grails.plugins:db-reverse-engineer:4.0.0
---
+++ grails-app/conf/spring/application.groovy
@@ -0,0 +1,3 @@
+grails.plugin.reveng.packageName = 'id.infomedia'
+grails.plugin.reveng.versionColumns = [other: 'nonstandard_version_name']
+grails.plugin.reveng.excludeTables = ['mail_contact']
